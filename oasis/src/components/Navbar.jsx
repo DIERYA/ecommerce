ace9ff2bb173eaf5159dcac1592776488df550b0
DIERYA <darkchinease@gmail.com>
2023-06-24 10:48:17 -0500

    Started React Project

---
+++ oasis/src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+
+// HomeWork: Create basic navbar for our eco-site (bootswatch)
+// Create Navbar component in this file
+// Import it into our main App.js file
+// Use example inside the App.js HTML: <Navbar />
