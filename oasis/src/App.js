cbd3e8f98c78436c62714b05e8138c81f4677540
DIERYA <darkchinease@gmail.com>
2023-07-09 12:00:04 -0500

    Added CSS, Images, and Title

--- oasis/src/App.js
+++ oasis/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Navbar from './components/Navbar';
-
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import logo from "./images/logo-transparent-png.png";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header"></header>
+      <div className="title">
+        <img src={logo} alt="Oasis"></img>
+        <h1>Revolutionary Marketplace</h1>
+      </div>
     </div>
   );
 }
@@ -14,3 +16,9 @@ function App() {
 export default App;
 
 
+// HomeWork:
+// The'title' div currently has two things:
+// logo & Text
+// We want the logo to the left and text to the right
+// Resize image to match text size
+// center on page and have some padding from navbar t
