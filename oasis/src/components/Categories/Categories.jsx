9b1e50c63d44bada3b31b2c340ee803f3bd771a6
gitFaisal <faisamalik34@gmail.com>
2023-07-31 20:34:22 -0400

    Updated file structure, added styles for categories

--- oasis/src/components/Categories/Categories.jsx
+++ oasis/src/components/Categories/Categories.jsx
@@ -1,49 +1,46 @@
-import React from 'react'
-import  './CategoryStyles.css'
+import React from 'react';
+import './CategoryStyles.css';
 
 const Categroies = () => {
-    return (
-
-        <div className="container container-nav">
-          <ul>
-            <li className="border-white" id="open-nav-sidebar">
-              <span className="open-nav-slider">
-                <i className="fa-solid fa-bars"></i>
-                All
-              </span>
-            </li>
-            <li className="border-white">
-              <a href="#">Best Sellers</a>
-            </li>
-            <li className="border-white">
-              <a href="#">Today's Deals</a>
-            </li>
-            <li className="border-white">
-              <a href="#">Mobiles</a>
-            </li>
-            <li className="border-white">
-              <a href="#">Customer Service</a>
-            </li>
-            <li className="border-white">
-              <a href="#">Electronic</a>
-            </li>
-            <li className="border-white">
-              <a href="#">Home & Kitchen</a>
-            </li>
-            <li className="border-white">
-              <a href="#">Fashion</a>
-            </li>
-            <li className="border-white">
-              <a href="#">Book</a>
-            </li>
-            <li className="border-white prime-image-hover">
-              <a href="#">Prime</a>
-         
-            </li>
-          </ul>
-      
-        </div>
-    )
-}
+  return (
+    <div className='tab-bar'>
+      <ul>
+        <li id='open-nav-sidebar'>
+          <span className='open-nav-slider'>
+            <i className='fa-solid fa-bars'></i>
+            All
+          </span>
+        </li>
+        <li>
+          <a href='#'>Best Sellers</a>
+        </li>
+        <li>
+          <a href='#'>Today's Deals</a>
+        </li>
+        <li>
+          <a href='#'>Mobiles</a>
+        </li>
+        <li>
+          <a href='#'>Customer Service</a>
+        </li>
+        <li>
+          <a href='#'>Electronic</a>
+        </li>
+        <li>
+          <a href='#'>Home & Kitchen</a>
+        </li>
+        <li>
+          <a href='#'>Fashion</a>
+        </li>
+        <li>
+          <a href='#'>Book</a>
+        </li>
+        <li className='border-white prime-image-hover'>
+          <a href='#'>Prime</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Categroies;
