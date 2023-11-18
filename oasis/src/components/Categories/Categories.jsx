4c34e77b5cac2111674144ca1cbdb3a978e0252f
gitFaisal <faisamalik34@gmail.com>
2023-11-18 12:05:28 -0500

    Fix scroll tab bar height and styles

--- oasis/src/components/Categories/Categories.jsx
+++ oasis/src/components/Categories/Categories.jsx
@@ -1,42 +1,36 @@
-import React from "react";
-import "./CategoryStyles.css";
+import React from 'react';
+import './CategoryStyles.css';
 
 const Categroies = () => {
   return (
-    <div className="tab-bar">
-      <ul class="horizontal-scroll">
-        <li id="open-nav-sidebar">
-          <span className="open-nav-slider">
-            <i className="fa-solid fa-bars"></i>
-            All
-          </span>
+    <div className='tab-bar'>
+      <ul class='horizontal-scroll'>
+        <li>
+          <a href='#'>Best Sellers</a>
         </li>
         <li>
-          <a href="#">Best Sellers</a>
+          <a href='#'>Today's Deals</a>
         </li>
         <li>
-          <a href="#">Today's Deals</a>
+          <a href='#'>Mobiles</a>
         </li>
         <li>
-          <a href="#">Mobiles</a>
+          <a href='#'>Customer Service</a>
         </li>
         <li>
-          <a href="#">Customer Service</a>
+          <a href='#'>Electronic</a>
         </li>
         <li>
-          <a href="#">Electronic</a>
+          <a href='#'>Home & Kitchen</a>
         </li>
         <li>
-          <a href="#">Home & Kitchen</a>
+          <a href='#'>Fashion</a>
         </li>
         <li>
-          <a href="#">Fashion</a>
+          <a href='#'>Book</a>
         </li>
-        <li>
-          <a href="#">Book</a>
-        </li>
-        <li className="border-white prime-image-hover">
-          <a href="#">Prime</a>
+        <li className='border-white prime-image-hover'>
+          <a href='#'>Prime</a>
         </li>
       </ul>
     </div>
